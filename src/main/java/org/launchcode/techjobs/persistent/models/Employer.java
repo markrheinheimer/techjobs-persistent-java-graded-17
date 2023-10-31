cc294f0b473368b96a2d809ef0ab7b5ffd4b6f31
[Mark Rheinheimer] <markrheinheimer@protonmail.comgit config --global user.name markrheinheimer>
2023-10-30 19:58:53 -0500

    persistent stuidio task 1 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,9 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Employer extends AbstractEntity {
+
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,9 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Skill extends AbstractEntity {
+
 
 }
